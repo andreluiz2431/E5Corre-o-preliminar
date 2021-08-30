01045bed3f9ef3fc810aa00704fc32775a554c12
André Luiz <42250150+andreluiz2431@users.noreply.github.com>
2021-08-30 16:58:27 +0000

    Initial commit

---
+++ clientUDP.py
@@ -0,0 +1,80 @@
+import socket # importando socket
+import sys # importando sys para receber por parametros
+import hashlib # importando hashlib
+import time
+
+params = sys.argv[1:] # definindo variável igual aos parâmetros
+
+print("\nInicializando socket...")
+socketClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("\nSocket inicializado.")
+
+if(len(sys.argv) > 1):
+    # Criando HELP da aplicação
+    if(params[0] == "--help" or params[0] == "-h"):
+        print("Execucao do cliente: python3 <nomeArquivo.py> <entradaDadosArquivo> <tamanhoQuadros> <ip> <porta>")
+        print("-------------------- Ex: python3 clientUDP.py enviar.txt 7 127.0.0.1 12000 --------------------")
+    
+    else:
+        nameArqSend = params[0] # definindo variável para o primeiro parâmetro
+        
+        print("\nLendo arquivo...")
+        arqSend = open(nameArqSend, 'r') # ler aquivo para enviar dados e definir na variavel
+        print("\nArquivo lido.")
+        
+        tamQuadros = int(params[1]) # definindo variável para o parâmetro de tamanho de quadros
+        
+        serverName = params[2] # definindo variável para o parâmetro de ip
+        
+        serverPort = int(params[3]) # definindo variável para o parâmetro da porta
+
+        hostAndress = (serverName, serverPort) # definindo endereço completo 
+
+        hash = hashlib.md5() # encriptando
+        
+        socketClient.settimeout(1) # atraso
+
+        nSeq = 1
+
+        for linha in arqSend:
+            i = 0
+            while(i < len(linha) - 1):
+
+                message = linha[i:(i+tamQuadros)] # particionando mensagerm para envio
+
+                hash.update(str(nSeq).encode())
+                hashcod = hash.hexdigest() # Transforma o codigo em hash
+
+                msgSend  = str(nSeq) + ' ' + hashcod + ' ' + "{}".format(message)  # Junta o numero do frame com o sequencial e a mensagem
+
+
+                if(socketClient.sendto(msgSend.encode(),(hostAndress))) : # faz o envio do conjunto
+          
+                    try :
+                
+                        request, clientAddress = socketClient.recvfrom(1024) # confirmação de envio
+                      
+                        if(request == b'ACK') :
+                            print(request.decode('utf-8')) # ACK
+                            print("Frame salvo!")
+                                        
+                        elif(request == b'NACK') :
+                            print(request.decode('utf-8')) # NACK
+                            print("Falha no frame: "+str(nSeq)) # frame deu erro
+                        
+                    except:
+                        print("Err: Timeout") # excedeu o tempo e fecha e para de enviar os frames
+                        break 
+                
+                print("Pacote enviado: ", message)
+                i = (i + tamQuadros) # pula pelo tamnho do quadro | prox. quadro
+          
+                nSeq += 1 # vai pra a prox. sequencia
+
+        message = str.encode("ok") # definindo mensagem de confirmação de dados enviados
+        
+        socketClient.sendto(message, hostAndress) # enviando mensagem de confirmação para o servidor
+
+        print("\nAplicação finalizada.")
+        arqSend.close() # fechando arquivo
+        socketClient.close() # finalizando socket
---
+++ serverUDP.py
@@ -0,0 +1,54 @@
+import socket # importando socket
+import sys # importando sys para receber por parametros
+import hashlib # importando hashlib
+
+params = sys.argv[1:] # definindo variável igual aos parâmetros
+
+if(len(sys.argv) > 1):
+    # Criando HELP da aplicação
+    if(params[0] == "--help" or params[0] == "-h"):
+        print("Execucao do servidor: python3 <nomeArquivo.py> <porta> <nomeArquivoSaida>")
+        print("--------------- Ex: python3 serverUDP.py 12000 saida.txt ---------------")
+    
+    else:
+        serverPort = int(params[0]) # definindo variável para o parâmetro da porta
+        
+        nameArqSave = params[1] # definindo variável para o parâmetro que recebe nome do arquivo para salvar
+        
+        arqSave = open(nameArqSave, 'w') # sinaliza para escrever no aquivo para salvar dados e definir na variavel
+
+        socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # inicializando socket UDP
+        
+        socketServer.bind(('', serverPort)) # recebendo conecção do socket
+
+        hash = hashlib.md5()
+
+        print('Servidor ligado...')
+
+        while 1:
+            message, clientAddress = socketServer.recvfrom(2048) # recebendo mensagens do cliente
+
+            # se receber mensagem de confirmação do cliente que os dados foram enviados
+            if(message == b'ok'):
+                print("Dados recebidos.")
+                arqSave.close() # fechando arquivo
+            else:
+
+                msgSend = message.decode('utf-8').split(' ',2) # Divide a string nos dois primeiros espaços
+                nSeq = msgSend[0] # Recebe o numero de sequencia provinda do transmissor
+                hashcod = msgSend[1]# Recebe o codigo de verificação do transmissor
+                cargaUtil = msgSend[2]# Recebe a carga util provinda do transmissor    
+
+                hash.update(nSeq.encode())
+                nHash = hash.hexdigest() # Transforma em hash md5
+
+                if(nHash == hashcod):#Verifica se o numero de sequencia e o codigo de verifcação condizem com o esperado
+           
+                    arqSave.writelines(cargaUtil) # Escreve a mensagem contida no frame no arquivo de saída
+                    socketServer.sendto(str.encode('ACK'),clientAddress)#envia para o transmissor que tudo foi verificado e está OK          
+                    
+                else: 
+                   
+                    socketServer.sendto(str.encode('NACK'),clientAddress) #Envia para o transmissor que houve um NACK
+
+socketServer.close()
